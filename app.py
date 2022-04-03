589bb02acf5a3606b1fc39de2302ae0cec21c639
Randil Tennakoon <randil2016al@gmail.com>
2022-04-03 20:13:53 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from distutils.log import debug
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/view')
+def view():
+    return render_template('view.html')
+
+
+if __name__ == '__main__':
+    app.route(debug=True)
+
+
+
